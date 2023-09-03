aba5d1f5dc273e638f2a3aa7903803bc083deee8
PaoloCaldera <paolo.caldera@outlook.com>
2023-09-03 16:06:40 +0200

    feature: implement recycler view adapter
    Create the header and the list item view holder for inflating the layout and binding the data respectively; assign a different view type according to the header or list item view.
    To increase performance, use the DiffUtil class to compare old and new recycler view.
    
    Integrates: #5

--- app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
@@ -1,27 +1,109 @@
 package com.example.android.politicalpreparedness.election.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.politicalpreparedness.databinding.ElectionListHeaderBinding
+import com.example.android.politicalpreparedness.databinding.ElectionListItemBinding
 // import com.example.android.politicalpreparedness.databinding.ViewholderElectionBinding
 import com.example.android.politicalpreparedness.network.models.Election
 
-/*
-class ElectionListAdapter(private val clickListener: ElectionListener)
-    : ListAdapter<Election, ElectionViewHolder>(ElectionDiffCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElectionViewHolder {
-        return ElectionViewHolder.from(parent)
+class ElectionListAdapter(
+    private val listTitle: String,
+    private val clickListener: ElectionListener
+) :
+    ListAdapter<Election, RecyclerView.ViewHolder>(ElectionDiffCallback) {
+
+    // Item types for recycler view
+    private val HEADER_VIEW_TYPE = 0
+    private val ITEM_VIEW_TYPE = 1
+
+    object ElectionDiffCallback : DiffUtil.ItemCallback<Election>() {
+        override fun areItemsTheSame(oldItem: Election, newItem: Election): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Election, newItem: Election): Boolean {
+            return oldItem == newItem
+        }
     }
 
-    // TODO: Bind ViewHolder
 
-    // TODO: Add companion object to inflate ViewHolder (from)
+    /**
+     * ViewHolder class for header of the recycler view
+     */
+    class ElectionListHeaderViewHolder private constructor(val binding: ElectionListHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            // Inflate the header layout and create the header view holder
+            fun from(parent: ViewGroup): ElectionListHeaderViewHolder {
+                return ElectionListHeaderViewHolder(
+                    ElectionListHeaderBinding.inflate(
+                        LayoutInflater.from(parent.context), parent, false
+                    )
+                )
+            }
+        }
+
+        // Bind the header view holder
+        fun bind(title: String) {
+            binding.apply {
+                electionListTitle = title
+                executePendingBindings()
+            }
+
+        }
+    }
+
+    /**
+     * ViewHolder class for the Election list items of the recycler view
+     */
+    class ElectionListItemViewHolder private constructor(val binding: ElectionListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            // Inflate the list item layout and create the item view holder
+            fun from(parent: ViewGroup): ElectionListItemViewHolder {
+                return ElectionListItemViewHolder(
+                    ElectionListItemBinding.inflate(
+                        LayoutInflater.from(parent.context), parent, false
+                    )
+                )
+            }
+        }
+
+        // Bind the list item view holder
+        fun bind(listItem: Election) {
+            binding.apply {
+                electionListItem = listItem
+                executePendingBindings()
+            }
+        }
+    }
+
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) HEADER_VIEW_TYPE else ITEM_VIEW_TYPE
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == HEADER_VIEW_TYPE)
+            ElectionListHeaderViewHolder.from(parent)
+        else
+            ElectionListItemViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder.itemViewType == ITEM_VIEW_TYPE)
+            (holder as ElectionListHeaderViewHolder).bind(listTitle)
+        else
+            (holder as ElectionListItemViewHolder).bind(getItem(position - 1))
+    }
 }
-
-// TODO: Create ElectionViewHolder
-
-// TODO: Create ElectionDiffCallback
 
 // TODO: Create ElectionListener
 
- */
