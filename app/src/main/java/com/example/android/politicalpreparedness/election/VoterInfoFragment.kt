e71a3050779286b72cd0c3ceedbca7a69d1ba06a
PaoloCaldera <paolo.caldera@outlook.com>
2023-09-08 17:16:10 +0200

    feature: initialize fragment binding and view model
    Develop the factory class of the view model and then create a view model instance inside the fragment. Also, inflate the fragment layout.
    
    Integrates: #7

--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -5,15 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.android.politicalpreparedness.database.ElectionDatabase
+import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
 
 class VoterInfoFragment : Fragment() {
+
+    private lateinit var binding: FragmentVoterInfoBinding
+    private val viewModel: VoterInfoViewModel by viewModels {
+        VoterInfoViewModel.VoterInfoViewModelFactory(ElectionDatabase.getInstance(requireContext()).electionDao)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?)
-    : View? {
-
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentVoterInfoBinding.inflate(inflater)
+        binding.apply {
+            lifecycleOwner = this@VoterInfoFragment
+            voterInfoViewModel = viewModel
+        }
         // TODO: Add ViewModel values and create ViewModel
 
         // TODO: Add binding values
@@ -22,13 +34,13 @@ class VoterInfoFragment : Fragment() {
 
         /**
         Hint: You will need to ensure proper data is provided from previous fragment.
-        */
+         */
 
         // TODO: Handle loading of URLs
 
         // TODO: Handle save button UI state
         // TODO: cont'd Handle save button clicks
-        return null
+        return binding.root
     }
 
     // TODO: Create method to load URL intents
--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.android.politicalpreparedness.election
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.politicalpreparedness.database.ElectionDao
+import java.lang.IllegalArgumentException
 
 class VoterInfoViewModel(private val dataSource: ElectionDao) : ViewModel() {
 
@@ -18,4 +20,12 @@ class VoterInfoViewModel(private val dataSource: ElectionDao) : ViewModel() {
      * Hint: The saved state can be accomplished in multiple ways. It is directly related to how elections are saved/removed from the database.
      */
 
+    @Suppress("UNCHECKED_CAST")
+    class VoterInfoViewModelFactory(private val dataSource: ElectionDao) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(VoterInfoViewModel::class.java))
+                return VoterInfoViewModel(dataSource) as T
+            throw IllegalArgumentException("Unknown view model class VoterInfoViewModel")
+        }
+    }
 }
