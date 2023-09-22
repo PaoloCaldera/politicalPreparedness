ff5d07c2c62f45eec2a5cb899a8713300dbd3eee
PaoloCaldera <paolo.caldera@outlook.com>
2023-09-22 15:53:03 +0200

    feature: hide or show the views according to the status
    When it is loading, show the loading image.
    When it has fetched all the data, show the recycler view with the corresponding data.
    When there is a connection error, show the cloud broken icon.
    
    Integrates: #11

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -29,6 +29,28 @@ class ElectionsFragment : Fragment() {
             electionsViewModel = viewModel
         }
 
+        // Observe LiveData variable that handles the layout based on the network result
+        viewModel.networkStatus.observe(viewLifecycleOwner) { apiStatus ->
+            when (apiStatus) {
+                CivicsApiStatus.LOADING -> binding.apply {
+                    upcomingRecyclerView.visibility = View.GONE
+                    connectionErrorImage.visibility = View.GONE
+                    loadingImage.visibility = View.VISIBLE
+                }
+                CivicsApiStatus.SUCCESS -> binding.apply {
+                    loadingImage.visibility = View.GONE
+                    connectionErrorImage.visibility = View.GONE
+                    upcomingRecyclerView.visibility = View.VISIBLE
+                }
+                CivicsApiStatus.ERROR -> binding.apply {
+                    upcomingRecyclerView.visibility = View.GONE
+                    loadingImage.visibility = View.GONE
+                    connectionErrorImage.visibility = View.VISIBLE
+                }
+                else -> throw Exception("Invalid HTTP connection status")
+            }
+        }
+
         // Observe LiveData variable aimed to handle navigation to VoterInfoFragment
         viewModel.navigateToVoterInfoFlag.observe(viewLifecycleOwner) {
             it?.let {
