411867af05d922c1f24b3252a752ef9e828a448c
PaoloCaldera <paolo.caldera@outlook.com>
2023-10-05 15:40:57 +0200

    feature: add date conversion functions in custom moshi adapter
    Define the conversion functions for the response element electionDay.
    Use the same adapter already exploited for the division parsing.
    
    Integrates: #17

--- app/src/main/java/com/example/android/politicalpreparedness/network/jsonadapter/ElectionAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/jsonadapter/ElectionAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.android.politicalpreparedness.network.jsonadapter
 
+import android.annotation.SuppressLint
 import com.example.android.politicalpreparedness.network.models.Division
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
+import java.text.SimpleDateFormat
+import java.util.Date
 
+@SuppressLint("SimpleDateFormat")
 class ElectionAdapter {
 
     /**
@@ -24,5 +28,21 @@ class ElectionAdapter {
     @ToJson
     fun divisionToJson (division: Division): String {
         return division.id
+    }
+
+
+    /**
+     * Adapter methods for converting string typed dates into Date objects
+     */
+
+
+    @FromJson
+    fun dateFromJson (electionDay: String): Date {
+        return SimpleDateFormat("yyyy-MM-dd").parse(electionDay)!!
+    }
+
+    @ToJson
+    fun dateToJson (date: Date): String {
+        return SimpleDateFormat("yyyy-MM-dd").format(date)
     }
 }
