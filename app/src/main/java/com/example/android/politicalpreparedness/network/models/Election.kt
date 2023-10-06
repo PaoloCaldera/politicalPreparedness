af9ceb6e626c7a3e6cb865ed3bddc82bede6df59
PaoloCaldera <paolo.caldera@outlook.com>
2023-10-07 00:04:49 +0200

    refactor: let Election data class implement Parcelable
    Since an Election object must be passed as a safe argument from the ElectionsFragment to VoterInfoFragment, it must be parcelized.

--- app/src/main/java/com/example/android/politicalpreparedness/network/models/Election.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/models/Election.kt
@@ -1,13 +1,16 @@
 package com.example.android.politicalpreparedness.network.models
 
+import android.os.Parcelable
 import androidx.room.*
 import com.squareup.moshi.*
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
 @Entity(tableName = "election_table")
+@Parcelize
 data class Election(
-        @PrimaryKey val id: Int,
-        @ColumnInfo(name = "name") val name: String,
-        @ColumnInfo(name = "electionDay") val electionDay: Date,
-        @Embedded(prefix = "division_") @Json(name="ocdDivisionId") val division: Division
-)
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "electionDay") val electionDay: Date,
+    @Embedded(prefix = "division_") @Json(name = "ocdDivisionId") val division: Division
+) : Parcelable
