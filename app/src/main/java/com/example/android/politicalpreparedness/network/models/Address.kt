155502ba6b152074a09ea2432f7d7f197ce3e3d2
Paolo Caldera <paolo.caldera@outlook.com>
2023-10-17 15:39:54 +0200

    feature: use parcelization for some model classes
    As the Representative elements of the list must be saved inside the SavedStateHandle object, they must be parcelized in order to be retrieved at a later time.

--- app/src/main/java/com/example/android/politicalpreparedness/network/models/Address.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/models/Address.kt
@@ -1,12 +1,16 @@
 package com.example.android.politicalpreparedness.network.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Address(
     val line1: String,
     val line2: String? = null,
     val city: String,
     val state: String,
     val zip: String
-) {
+) : Parcelable {
     fun toFormattedString(): String {
         var output = line1.plus("\n")
         if (!line2.isNullOrEmpty()) output = output.plus(line2).plus("\n")
--- app/src/main/java/com/example/android/politicalpreparedness/network/models/Channel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/models/Channel.kt
@@ -1,6 +1,10 @@
 package com.example.android.politicalpreparedness.network.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Channel(
     val type: String,
     val id: String
-)
+) : Parcelable
